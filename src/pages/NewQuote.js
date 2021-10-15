ec141d789e58c159ec2a986e130a591c7c2b387c
Kavery pm <shiny.kavery@gmail.com>
2021-10-15 10:41:32 -0300

    added new quote form

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,4 +1,8 @@
+import QuoteForm from '../components/quotes/QuoteForm';
 const NewQuote = () => {
-  return <h1>New quotes</h1>;
-};
+    const addQuoteHandler = (quoteData)=>{
+        console.log(quoteData);
+    }
+  return <QuoteForm onAddQuote={addQuoteHandler}></QuoteForm>
+}
 export default NewQuote;
