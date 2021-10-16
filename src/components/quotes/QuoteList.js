45f5ae1453f99f9b12a47cc7f0bef1ae787a9509
Kavery pm <shiny.kavery@gmail.com>
2021-10-16 09:11:27 -0300

    added sorting function

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -4,6 +4,16 @@ import { useHistory, useLocation } from "react-router";
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
 
+const sortQuotes = (quotes, ascending) => {
+  return quotes.sort((quoteA, quoteB) => {
+    if (ascending) {
+      return quoteA.id > quoteB.id ? 1 : -1;
+    } else {
+      return quoteA.id < quoteB.id ? 1 : -1;
+    }
+  });
+};
+
 const QuoteList = (props) => {
   const history = useHistory();
   const location = useLocation();
@@ -12,12 +22,12 @@ const QuoteList = (props) => {
   const isSortAscending = queryParams.get("sort") === "asc";
 
   const changeSortingHandler = () => {
-    history.push("/quotes?sort=asc");
+    history.push("/quotes?sort="+(isSortAscending ? 'desc' : 'asc'));
   };
   return (
     <Fragment>
       <div className={classes.sorting}>
-        <button onClick={changeSortingHandler}>{isSortAscending ? 'Ascending' : 'Descending'}</button>
+        <button onClick={changeSortingHandler}>{isSortAscending ? 'Descending' : 'Ascending'}</button>
       </div>
       <ul className={classes.list}>
         {props.quotes.map((quote) => (
