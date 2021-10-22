6761dded4c40831e4aeeec5d3b3691fdf2a98dd4
Kavery pm <shiny.kavery@gmail.com>
2021-10-22 09:44:12 -0300

    Added quotelist using http hook

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,43 +1,43 @@
-const FIREBASE_DOMAIN = 'https://reacthook-f4714.firebaseio.com/';
-export const getAllQuotes =async ()=>{
-   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
-   const data = await response.json();
-   if(!response.ok) throw new Error(data.message || 'could not fetch quotes');
-const transformedQuotes = [];
-for(const key in data){
+const FIREBASE_DOMAIN = "https://reacthook-f4714.firebaseio.com/";
+export const getAllQuotes = async () => {
+  const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
+  const data = await response.json();
+  if (!response.ok) throw new Error(data.message || "could not fetch quotes");
+  const transformedQuotes = [];
+  for (const key in data) {
     const quoteObject = {
-        id: key,
-        ...data[key]
-    }
-    transformedQuotes.push(quoteObject)
+      id: key,
+      ...data[key],
+    };
+    transformedQuotes.push(quoteObject);
+  }
+  return transformedQuotes;
 };
-return transformedQuotes;
-}
 
-export const getSingleQuote = async(quoteId)=>{
-const response = await fetch(`${FIREBASE_DOMAIN}/quotes/${quoteId}.json`);
-const data = response.json();
-if(!response.ok) throw new Error(data.message || 'could not fetch quote');
-const loadedQuote = {
+export const getSingleQuote = async (quoteId) => {
+  const response = await fetch(`${FIREBASE_DOMAIN}/quotes/${quoteId}.json`);
+  const data = response.json();
+  if (!response.ok) throw new Error(data.message || "could not fetch quote");
+  const loadedQuote = {
     id: quoteId,
     ...data,
-}
-return loadedQuote;
-}
+  };
+  return loadedQuote;
+};
 
 export async function addQuote(quoteData) {
-    const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`, {
-      method: 'POST',
-      body: JSON.stringify(quoteData),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const data = await response.json();
-  
-    if (!response.ok) {
-      throw new Error(data.message || 'Could not create quote.');
-    }
-  
-    return null;
+  const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`, {
+    method: "POST",
+    body: JSON.stringify(quoteData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Could not create quote.");
   }
+
+  return null;
+}
--- src/pages/AllQuote.js
+++ src/pages/AllQuote.js
@@ -1,10 +1,35 @@
+import { useEffect } from 'react';
 import QuoteList from '../components/quotes/QuoteList';
+import LoadingSpinner from '../components/UI/LoadingSpinner';
+import useHttp from '../hooks/use-http';
+import { getAllQuotes } from '../lib/api';
+import NotFound from './not-found';
+
 const AllQuote = () => {
-    const DUMMY_QUOTES = [
-        { id: 'q1', author: 'shiny', text: 'Learning React is fun!' },
-        { id: 'q2', author: 'Kavery', text: 'Learning React is great!' },
-      ];
-     
-  return <QuoteList quotes={DUMMY_QUOTES}></QuoteList>
+  
+  const{sendRequest, status, data: loadedQuote, error}=useHttp(getAllQuotes, true);
+    // const DUMMY_QUOTES = [
+    //     { id: 'q1', author: 'shiny', text: 'Learning React is fun!' },
+    //     { id: 'q2', author: 'Kavery', text: 'Learning React is great!' },
+    //   ];
+     useEffect(() => {
+       sendRequest();
+       
+     }, [sendRequest]);
+     if(status==='pending'){
+       return (
+         <div className='centered'>
+           <LoadingSpinner/>
+         </div>
+       )
+     }
+     if(error){
+       return <p className='centered focussed'>{error}</p>
+     }
+     if(status==='completed'&& (!loadedQuote || loadedQuote.length === 0)){
+       return <NotFound/>
+     }
+  return <QuoteList quotes={loadedQuote}></QuoteList>
 };
 export default AllQuote;
+ 
