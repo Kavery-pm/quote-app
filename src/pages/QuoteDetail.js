60a92010ddbbacb23447f504ecd50616da8f16e5
Kavery pm <shiny.kavery@gmail.com>
2021-10-22 00:13:12 -0300

    added route match

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, useParams, Link } from "react-router-dom";
+import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 const DUMMY_QUOTES = [
@@ -8,20 +8,22 @@ const DUMMY_QUOTES = [
 ];
 const QuoteDetail = () => {
   const params = useParams();
+  const match = useRouteMatch();
+  console.log(match);
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
   if (!quote) return <p>Quote not found</p>;
   return (
     <React.Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}`} exact>
+      <Route path={`${match.path}`} exact>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+          <Link className="btn--flat" to={`${match.path}/comments`}>
             Load comments
           </Link>
         </div>
       </Route>
 
-      <Route to={`/quotes/${params.quoteId}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments></Comments>
       </Route>
     </React.Fragment>
