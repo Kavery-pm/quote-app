c06ffde1d3904327c783ef8e3f3b2f013362663a
Kavery pm <shiny.kavery@gmail.com>
2021-10-22 02:00:35 -0300

    added new quotes api

--- src/lib/api.js
+++ src/lib/api.js
@@ -13,3 +13,31 @@ for(const key in data){
 };
 return transformedQuotes;
 }
+
+export const getSingleQuote = async(quoteId)=>{
+const response = await fetch(`${FIREBASE_DOMAIN}/quotes/${quoteId}.json`);
+const data = response.json();
+if(!response.ok) throw new Error(data.message || 'could not fetch quote');
+const loadedQuote = {
+    id: quoteId,
+    ...data,
+}
+return loadedQuote;
+}
+
+export async function addQuote(quoteData) {
+    const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`, {
+      method: 'POST',
+      body: JSON.stringify(quoteData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await response.json();
+  
+    if (!response.ok) {
+      throw new Error(data.message || 'Could not create quote.');
+    }
+  
+    return null;
+  }
